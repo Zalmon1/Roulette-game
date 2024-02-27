07a47673e3aef235dba59ee33f60701c50258834
Enzo saaristo <enzo.rivassaaristo@elev.ga.lbs.se>
2024-02-27 10:40:01 +0100

    card

--- Assets/Scripts/CardScript.cs
+++ Assets/Scripts/CardScript.cs
@@ -1,43 +1,12 @@
-using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.ExceptionServices;
 using UnityEngine;
 
-public class CardScript : MonoBehaviour
+[CreateAssetMenu(fileName = "card", menuName = "Deck/Card")]
+public class CardScript : ScriptableObject
 {
-
-    [SerializeField] int chance = 50;
-
+   public int Value = 0;
 
 
-    private void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            Randomize();
-        }
-    }
-
-    public void Randomize()
-    {
-        int luck = Random.Range(1, 100);
-
-        if (chance >= luck)
-        {
-            // You get the card
-
-
-        }
-        else
-        {
-            // You dont get the card
-
-        }
-    }
-
-    void Cards()
-    {
-
-    }
 }
+
---
+++ Assets/Scripts/DeckScript.cs
@@ -0,0 +1,41 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Deck", menuName = "Deck/Deck")]
+public class DeckScript : ScriptableObject
+{
+    public List<CardScript> Deck = new List<CardScript>();
+    public List<CardScript> ShuffledDeck;
+
+    [SerializeField] int chance = 50;
+
+    public void Randomize()
+    {
+        ShuffledDeck = new List<CardScript>();
+        foreach (CardScript card in Deck)
+        {
+            ShuffledDeck.Add(Instantiate(card));
+        }
+     
+        int luck = UnityEngine.Random.Range(1, 100);
+
+        if (chance >= luck)
+        {
+            // You get the card
+
+
+        }
+        else
+        {
+            // You dont get the card
+
+        }
+    }
+
+    
+}
