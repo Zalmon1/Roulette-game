9aecb8d6d9b0720ff43949cf8fbfe8b46b02f181
Samuel Nordquist <samuel.nordquist@learnet.se>
2024-03-08 08:30:24 +0100

    booler

--- Assets/Scripts/SamuelRouletteScript.cs
+++ Assets/Scripts/SamuelRouletteScript.cs
@@ -8,54 +8,17 @@ public class SamuelRouletteScript : MonoBehaviour
 {
     [SerializeField] int PlayerHealth;
     [SerializeField] int EnemyHealth;
+    [SerializeField] bool YourTurn;
 
-
-   [SerializeField] bool YourTurn = true;
-
-    void Start()
+    
+    public void PlayerTakeDamage()
     {
-        EnemyHealth = 10;
-        PlayerHealth = 10;
-
-        
+        PlayerHealth--;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void EnemyTakeDamage()
     {
-        
-    }
- 
-    public void EnemyRandomShoot()
-    {
-        float RandomEnemyShot = Random.Range(0f, 5f);
-
-        if (RandomEnemyShot < 0f)
-    }
-
-    public void PlayerRandomShoot()
-    {
-        float RandomShot = Random.Range(0f, 5f);
-
-        if (RandomShot > 1f && YourTurn == true) // check if the value is larger than 1, if its true you don't hit
-        {
-            Debug.Log("You missed the enemy");
-
-            YourTurn = false;
-
-            return;
-        }
-        else if (RandomShot < 1f && YourTurn == true) // else if random is smaller than one, you get to shoot
-        {
-            Debug.Log("You hit the enemy");
-
-            YourTurn = false;
-
-            EnemyHealth--;
-
-            return; 
-        }
-
+        EnemyHealth--;
     }
 
 
@@ -63,50 +26,5 @@ public class SamuelRouletteScript : MonoBehaviour
 
 
 
-    public void RandomPlayerShoot()
-    {
-       
-
-        if (Random.Range(0f, 5f) <= 1 && YourTurn == true)
-        {
-            EnemyHealth--;
-            
-            YourTurn = false;
-            Debug.Log("You hit the enemy");
-        }
-        else
-        {
-            YourTurn = false;
-            Debug.Log("You didn't hit the enemy");
-            return;
-        }
-
-
-
-
-
-    }
-    public void RandomEnemyShoot()
-    {
-
-
-        float randomNumber = Random.Range(0f, 5f);
-        //Debug.Log(randomNumber);
-        if (randomNumber < 1 && YourTurn == false)
-        {
-            PlayerHealth--;
-            YourTurn = true;
-           Debug.Log("The enemy hit you");
-        }
-        else if(randomNumber > 1 && YourTurn == false)
-        {
-            YourTurn = true;
-            Debug.Log("The enemy missed you");
-            return;
-        }
-
-
-    }
-   
 
 }
