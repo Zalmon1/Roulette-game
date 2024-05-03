923ded4a9106687a48e9852bef3710ba7a589909
Samuel Nordquist <samuel.nordquist@learnet.se>
2024-05-03 09:13:48 +0200

    kimmich

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -10,28 +10,57 @@ public class HealthScript : MonoBehaviour
     
     private int healthOfCharacter;
 
+    private static int trackHealthOfPlayer1;
+
+    private static int trackHealthOfPlayer2;
+
+    // currentImage.sprite = allHeartsSprites[3];  
+
+
     // Start is called before the first frame update
     void Start()
     {
             
-            
-
-        currentImage.sprite = allHeartsSprites[3];  
+        
     }
 
+    void Update()
+    {
+        trackHealthOfPlayer2 = RoundScript.player2Health;
+        trackHealthOfPlayer1 = RoundScript.player1Health;
+    }
 
-    public void ChageSpriteOfHeart() { 
+   // public void ChangeSpriteOfHeart() { 
         
-        if(healthOfCharacter == 3)
+      //  if(healthOfCharacter == 3)
+       // {
+       //     currentImage.sprite = allHeartsSprites[2];
+       // }
+   
+   // }
+
+    public void CheckHealthOfPlayer1()
+    {
+        if (trackHealthOfPlayer1 ==  4)
+        {
+            currentImage.sprite = allHeartsSprites[0];
+        }
+        else if (trackHealthOfPlayer1 == 3)
+        {
+            currentImage.sprite = allHeartsSprites[1];
+        }
+        else if (trackHealthOfPlayer1 == 2)
         {
             currentImage.sprite = allHeartsSprites[2];
         }
-        
+        else if (trackHealthOfPlayer1 == 1)
+        {
+            currentImage.sprite = allHeartsSprites[3];
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void CheckHealthOfPlayer2() 
+    { 
+    
     }
 }
